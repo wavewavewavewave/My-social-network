0b45c3a7a33a0ce415eed243624828428c065a0c
wavewavewavewave <nikitashkrabov@gmail.com>
2021-11-22 20:07:00 +0300

    add dispatch to profile

--- src/index.tsx
+++ src/index.tsx
@@ -3,24 +3,25 @@ import {BrowserRouter} from "react-router-dom";
 import {App} from "./App";
 import React from "react";
 import {Provider} from "react-redux";
-import  storeOld from "./redux/store";
+import storeOld from "./redux/store";
 import {StateType} from "./redux/store";
 import {store} from "./redux/reduxStore";
 
 
 // export const rerenderEntireTree = () => {
-    ReactDOM.render(
-        <Provider store={store}>
-        <BrowserRouter>
-            <App store={storeOld}
-                 dispatch={storeOld.dispatch.bind(storeOld)}
-                 newPostText={storeOld._state.profilePage.newPostText}/>
-        </BrowserRouter>
-        </Provider> , document.getElementById('root'));
-// }
 
-// rerenderEntireTree(store.getState());
-// store.subscribe(() => {
-//     let state = store.getState()
-//     rerenderEntireTree(state)
-// });
+ReactDOM.render(
+    <Provider store={store}>
+        <BrowserRouter>
+        <App store={storeOld}
+             dispatch={storeOld.dispatch.bind(storeOld)}
+             newPostText={storeOld._state.profilePage.newPostText}/>
+        </BrowserRouter></Provider> , document.getElementById('root'));
+
+
+      {/*  rerenderEntireTree(store.getState());
+        store.subscribe(() => {
+        let state = store.getState()
+        rerenderEntireTree(state)
+    });*/}
+
